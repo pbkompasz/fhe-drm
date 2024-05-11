08e769c07348c05f581ffc7ffa6d58b572dc88f0
Peter Kompasz <kompaszbence@gmail.com>
2024-05-11 22:10:40 +0300

    add menu

--- drm/src/main.tsx
+++ drm/src/main.tsx
@@ -1,15 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router.tsx';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.tsx";
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { amber, grey } from "@mui/material/colors";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: amber,
+    divider: amber[200],
+    background: {
+      default: grey[900],
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Stack style={{ minHeight: "100vh" }} alignItems="flex-start">
+        <Stack direction="row" gap={10} alignSelf="center">
+          <a href="/">Home</a>
+          <a href="/record">View created records</a>
+          <a href="/collection">Manage collections</a>
+          <a href="/create">Create new record</a>
+        </Stack>
+        <Divider />
+        <RouterProvider router={router} />
+      </Stack>
+    </ThemeProvider>
+  </React.StrictMode>
+);
