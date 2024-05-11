e89c7c35dfbd17752b5edf7e751eac52fa1c346f
Peter Kompasz <kompaszbence@gmail.com>
2024-05-11 22:10:19 +0300

    add routes

--- drm/src/router.tsx
+++ drm/src/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
 import Collection from "./components/collection/Collection";
+import Create from "./components/create/Create";
+import Record from "./components/record/Record";
 
 export const router = createBrowserRouter([
   {
@@ -8,7 +10,15 @@ export const router = createBrowserRouter([
     element: <App />,
   },
   {
-    path: "/collection/:id",
-    element: <Collection></Collection>
+    path: "/create",
+    element: <Create />,
+  },
+  {
+    path: "/collection/:id?",
+    element: <Collection></Collection>,
+  },
+  {
+    path: "/record/:id?",
+    element: <Record></Record>,
   },
 ]);
