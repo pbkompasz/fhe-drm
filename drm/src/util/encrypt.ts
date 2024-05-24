6e5e17872557cb8c8cbadbd276586822686e94ff
Peter Kompasz <kompaszbence@gmail.com>
2024-05-24 23:44:25 +0300

    encrypt fix

--- drm/src/util/encrypt.ts
+++ drm/src/util/encrypt.ts
@@ -64,17 +64,79 @@ const encryptFile = async (file: File, seed: number) => {
   return new File([blob], "file.name");
 };
 
-const decryptFile = async (file: File, seed: number) => {
-  const fileArrayBuffer = await readFileAsArrayBuffer(file);
-  const key = await importKey(seed);
-  const iv = await generateIV(seed);
-  const encryptedData = await window.crypto.subtle.decrypt(
-    { name: "AES-CBC", iv },
-    key,
-    fileArrayBuffer
+const _stringToArrayBuffer = (str: string) => {
+  const encoder = new TextEncoder();
+  return encoder.encode(str).buffer;
+};
+
+const _digestMessage = async (message: string) => {
+  const data = _stringToArrayBuffer(message);
+  const hash = await crypto.subtle.digest("SHA-256", data);
+  return hash;
+};
+
+const _arrayBufferToHexString = (buffer: ArrayBuffer) => {
+  const byteArray = new Uint8Array(buffer);
+  const hexCodes = [...byteArray].map((value) => {
+    const hexCode = value.toString(16);
+    const paddedHexCode = hexCode.padStart(2, "0");
+    return paddedHexCode;
+  });
+
+  return hexCodes.join("");
+};
+
+export const getKeyFromPassphrase = async (passphrase: string) => {
+  const key = await _digestMessage(passphrase);
+  const keyHex = _arrayBufferToHexString(key);
+  return keyHex;
+};
+
+export const getIvFromPassphrase = async (passphrase: string) => {
+  const keyHex = await getKeyFromPassphrase(passphrase);
+  const ivHex = keyHex.substring(0, 32);
+  return ivHex;
+};
+
+const _arrayBufferFromHexString = (hexString: string) => {
+  const bytes = Uint8Array.from(
+    hexString.match(/.{1,2}/g).map((byte: any) => parseInt(byte, 16))
   );
-  const blob = new Blob([encryptedData]);
-  return new File([blob], file.name);
+  return bytes.buffer;
+};
+
+const decryptFile = async (file: File, seed: number) => {
+  try {
+    const fileArrayBuffer = await readFileAsArrayBuffer(file);
+    console.log(fileArrayBuffer.byteLength);
+
+    const keyHex = await getKeyFromPassphrase(String(seed));
+    const ivHex = await getIvFromPassphrase(String(seed));
+
+    const ivArrayBuffer = _arrayBufferFromHexString(ivHex);
+    const keyArrayBuffer = _arrayBufferFromHexString(keyHex);
+
+    const secretKey = await crypto.subtle.importKey(
+      "raw",
+      keyArrayBuffer,
+      {
+        name: "AES-CBC",
+        length: 256,
+      },
+      true,
+      ["encrypt", "decrypt"]
+    );
+
+    const encryptedData = await window.crypto.subtle.decrypt(
+      { name: "AES-CBC", iv: ivArrayBuffer },
+      secretKey,
+      fileArrayBuffer
+    );
+    const blob = new Blob([encryptedData]);
+    return new File([blob], file.name);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export { encryptFile, decryptFile };
